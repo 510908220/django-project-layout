8aa3510f1c7f86314fe7ddbb00c57f9183153aa1
westdoorblowcola <510908220@qq.com>
2016-07-27 21:18:54 +0800

    update

--- django-project-template/userapps/ui/urls.py
+++ django-project-template/userapps/ui/urls.py
@@ -1,20 +1,8 @@
 from django.conf.urls import url
 
-from .views import *
+from .views import index
 
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^project/$', project, name='project'),
-    url(r'^collect/$', collect,
-        name='collect'),
-    url(r'^summary/$', summary, name='summary'),
-    url(r'^email/$', email, name='email'),
-    url(r'^content/$', content, name='content'),
-    url(r'^commit/$', commit, name='commit'),
-    url(r'^upgrade/$', upgrade, name='upgrade'),
-    url(r'^jenkins/$', jenkins, name='jenkins'),
-    url(r'^shell/$', shell, name='shell'),
-    url(r'^schedule/$', schedule, name='schedule'),
-    url(r'^publishing/$', publishing, name='publishing'),
+    url(r'^$', index, name='index')
 ]
