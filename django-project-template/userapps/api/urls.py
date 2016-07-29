761db43986d23d5c2d6171056134e3d2a3e0c0a3
westdoorblowcola <510908220@qq.com>
2016-07-29 13:26:12 +0800

    url配置错误修改.

--- django-project-template/userapps/api/urls.py
+++ django-project-template/userapps/api/urls.py
@@ -8,8 +8,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ShopList, ShopDetail
 
 urlpatterns = [
-    url(r'shops/', ShopList.as_view(), name='shop_list'),
-    url(r'shops/(?P<pk>[0-9]+)/', ShopDetail.as_view(), name='shop_detail'),
+    url(r'shops/$', ShopList.as_view(), name='shop_list'),
+    url(r'shops/(?P<pk>[0-9]+)/$', ShopDetail.as_view(), name='shop_detail'),
 ]
-
 urlpatterns = format_suffix_patterns(urlpatterns)
