dfea15a29ed0332e357a6facb600a507ed2f5766
westdoorblowcola <510908220@qq.com>
2016-08-09 18:09:47 +0800

    练习信号

--- userapps/api/views.py
+++ userapps/api/views.py
@@ -12,6 +12,20 @@ logger = logging.getLogger("api")
 
 # 实际queryset 只是用于获取列表,不影响创建
 
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+
+# @receiver(pre_save, sender=Shop)
+def my_callback(sender, **kwargs):
+    instance = kwargs.get('instance')
+    if instance:
+        print "shop name is:", instance.name
+    print("oooooooooooooooooooooooooooo my_callback", sender, kwargs)
+
+pre_save.connect(my_callback, dispatch_uid="my_unique_identifier")
+pre_save.connect(my_callback, dispatch_uid="justdoit")
+# dispatch_uid可以标识函数，要不调用多次connect实际值会保存一个接受者.
 
 class ShopList(generics.ListCreateAPIView):
     """
